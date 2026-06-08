d81fa732fff693f080a6b41e678d25c14ca95422
ht <htht0823@qq.com>
2026-06-08 17:52:29 +0800

    fix: 导入 computed 修复 TS 编译错误

--- taoling-gallery/src/views/admin/AdminImages.vue
+++ taoling-gallery/src/views/admin/AdminImages.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, reactive, ref } from 'vue'
+import { computed, onMounted, reactive, ref } from 'vue'
 import { ElMessage } from 'element-plus'
 import { Delete, Edit, RefreshLeft, RefreshRight, Search, View } from '@element-plus/icons-vue'
 
